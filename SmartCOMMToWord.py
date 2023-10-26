b176619bb7d0bdddd38a5cfa9035eb68f90102fe
Vinoth Rajalingam <vinoth.lingam333@gmail.com>
2023-10-26 15:38:32 -0500

    New chages with UI

--- SmartCOMMToWord.py
+++ SmartCOMMToWord.py
@@ -1,20 +1,34 @@
 import os
-import docx
-from xml.etree import ElementTree
 import base64
 from io import BytesIO
 from PIL import Image
 import xml.etree.ElementTree as ET
-from docx import Document
+import PySimpleGUI as sg
+import docx
 from docx.shared import Cm
 
 # Define the input and output directories
-xmlFileFolder = '/Users/vinothrajalingam/Desktop/Python/xmlFileFolder'
-newWordDocFolder = '/Users/vinothrajalingam/Desktop/Python/newWordDocFolder'
+xmlFileFolder = ''
+newWordDocFolder = ''
 
-# Create the output directory if it does not exist
-if not os.path.exists(newWordDocFolder):
-    os.makedirs(newWordDocFolder)
+# Define the layout of the GUI
+description = sg.Text('XML to Word Converter', font=('Helvetica', 20), justification='center')
+prerequisites = sg.Text('Prerequisites:\n\n1. Python 3.x\n2. PySimpleGUI\n3. Pillow\n4. python-docx\n\n', font=('Helvetica', 14), justification='center')
+instructions = sg.Text('Instructions:\n\n1. Select the XML file folder.\n2. Select the new Word document folder.\n3. Click the "Convert" button to convert the XML files to Word documents.\n\n', font=('Helvetica', 14), justification='center')
+description_box = sg.Column([[description]], element_justification='center')
+prerequisites_box = sg.Column([[prerequisites]], element_justification='center')
+instructions_box = sg.Column([[instructions]], element_justification='center')
+layout = [
+    [description_box],
+    [sg.Frame('Prerequisites', [[prerequisites_box]], element_justification='center')],
+    [sg.Frame('Instructions', [[instructions_box]], element_justification='center')],
+    [sg.Text('XML File Folder:'), sg.Input(xmlFileFolder, key='xmlFileFolder'), sg.FolderBrowse()],
+    [sg.Text('New Word Doc Folder:'), sg.Input(newWordDocFolder, key='newWordDocFolder'), sg.FolderBrowse()],
+    [sg.Button('Convert'), sg.Button('Exit')]
+]
+
+# Create the GUI window
+window = sg.Window('XML to Word Converter', layout, size=(900, 500))
 
 # Define a function to add images to the document
 def add_image(doc, image_data, width, height, format, checksum):
@@ -25,11 +39,21 @@ def add_image(doc, image_data, width, height, format, checksum):
     img.save("temp_image." + format)
     doc.add_picture("temp_image." + format, width=docx.shared.Inches(width/96), height=docx.shared.Inches(height/96))  # You may need to adjust the width and height
 
-# Loop through all the XML files in the input directory
-for xml_file in os.listdir(xmlFileFolder):
-    if xml_file.endswith('.xml'):
+# Define a function to convert the XML files to Word documents
+def convert_files(xmlFileFolder, newWordDocFolder):
+    # Create the output directory if it does not exist
+    if not os.path.exists(newWordDocFolder):
+        os.makedirs(newWordDocFolder)
+
+    # Check if there are any XML files in the input directory
+    xml_files = [f for f in os.listdir(xmlFileFolder) if f.endswith('.xml')]
+    if not xml_files:
+        raise Exception('No XML files found in the input directory')
+
+    # Loop through all the XML files in the input directory
+    for xml_file in xml_files:
         # Open the XML file
-        tree = ElementTree.parse(os.path.join(xmlFileFolder, xml_file))
+        tree = ET.parse(os.path.join(xmlFileFolder, xml_file))
         root = tree.getroot()
 
         # Create a new Word document
@@ -86,3 +110,20 @@ for xml_file in os.listdir(xmlFileFolder):
 
         # Save the Word document
         doc.save(os.path.join(newWordDocFolder, os.path.splitext(xml_file)[0] + '.docx'))
+
+# Event loop for the GUI
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    elif event == 'Convert':
+        xmlFileFolder = values['xmlFileFolder']
+        newWordDocFolder = values['newWordDocFolder']
+        try:
+            convert_files(xmlFileFolder, newWordDocFolder)
+            sg.popup('Conversion complete!')
+        except Exception as e:
+            sg.popup_error(f'Error: {str(e)}')
+
+# Close the GUI window
+window.close()
